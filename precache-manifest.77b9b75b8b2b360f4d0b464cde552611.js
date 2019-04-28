b79a8404f915bfcc058e4c25ec9368fa11ac08bd
guitarbeard <alxdna@gmail.com>
2019-04-28 14:14:23 -0700

    Updates

---
+++ precache-manifest.77b9b75b8b2b360f4d0b464cde552611.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef6098a42153c8c381d5c019356fa2ad",
+    "url": "/asset-manifest.json"
+  },
+  {
+    "revision": "f827695106c228f3aefdffac62e200ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1587ccaf946599737d6d",
+    "url": "/static/css/main.a0959aa3.css"
+  },
+  {
+    "revision": "1587ccaf946599737d6d",
+    "url": "/static/js/main.1587ccaf.js"
+  }
+]);
