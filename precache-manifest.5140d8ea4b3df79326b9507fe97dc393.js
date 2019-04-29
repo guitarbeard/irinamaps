2a94867ce842d7ad57497220aa95f55d62639a21
guitarbeard <alxdna@gmail.com>
2019-04-29 00:19:18 -0700

    Updates

---
+++ precache-manifest.5140d8ea4b3df79326b9507fe97dc393.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f0c4cf6a5df302ba2662f3589c4c0a3",
+    "url": "/asset-manifest.json"
+  },
+  {
+    "revision": "a1cd08f547eb7b9bd2e94b2c4d66cd38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8024026948e3e272c2a",
+    "url": "/static/css/main.a0959aa3.css"
+  },
+  {
+    "revision": "c8024026948e3e272c2a",
+    "url": "/static/js/main.c8024026.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5559bdffbb902244c662cb3d1a399032.js"
+  "/precache-manifest.5140d8ea4b3df79326b9507fe97dc393.js"
 );
 
 self.addEventListener('message', (event) => {
